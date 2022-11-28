d9b65ba779d293f27c421c91b06233f817673bc1
Vighnesh <viggi.26@gmail.com>
2022-11-28 19:39:30 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import axios, * as others from 'axios'
 
 function App() {
+  const[artist ,setArtist] =  useState("")
+  const[song,setSong] = useState("")
+  const[lyrics,setLyrics] = useState("")
+  console.log(process.env.REACT_APP_API_KEY)
+  function searchLyrics() {
+    if(artist === ""|| song === ""){
+      return;
+    }
+    axios.get(
+      `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${song}&q_artist=${artist}&apikey=${process.env.REACT_APP_API_KEY}`).then(res => {
+            console.log(res.data)               
+                 
+              })
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Lyrics Finder</h1>
+        <input className='inp' placeholder='Artist name' onChange={(e)=>{setArtist(e.target.value)}}></input>
+
+        <input className='inp' placeholder='Song name' onChange={(e)=>{setSong(e.target.value)}}></input>
+
+        <button type='submit' onClick={()=>{searchLyrics()}}>Search</button>
+        <hr />
+
+        <pre>{lyrics}</pre>
+
     </div>
   );
 }
